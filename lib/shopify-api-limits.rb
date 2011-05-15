feb0d0931ef353c7f065bf6a9ba83ae160c03715
Chris Scott <christocracy@gmail.com>
2011-05-15 16:56:59 -0400

    Introduce error classes

--- lib/shopify-api-limits.rb
+++ lib/shopify-api-limits.rb
@@ -1,10 +1,24 @@
 $:.unshift File.dirname(__FILE__)
 
-require 'shopify-api-limits/active_resource/connection'
-require 'shopify-api-limits/shopify_api/shopify_api'
-
 module ShopifyAPI
   module Limits
+    # Connection hack
+    require 'shopify-api-limits/active_resource/connection'
     
+    require 'shopify-api-limits/shopify_api/shopify_api'
+    
+    class Error < StandardError
+      def self.status_code(code = nil)
+        return @code unless code
+        @code = code
+      end
+      
+      def status_code
+        self.class.status_code
+      end
+    end
+
+    class GlobalError < Error; status_code(1) ; end
+    class ShopError < Error; status_code(2) ; end
   end
 end
