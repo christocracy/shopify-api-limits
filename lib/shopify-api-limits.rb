e0a276459878874b1603d471403233c3c7142a21
Chris Scott <christocracy@gmail.com>
2011-05-15 15:19:56 -0400

    fix requires

--- lib/shopify-api-limits.rb
+++ lib/shopify-api-limits.rb
@@ -1,5 +1,7 @@
-require 'active_resource/connection'
-require 'shopify_api/shopify_api'
+$:.unshift File.dirname(__FILE__)
+
+require 'shopify-api-limits/active_resource/connection'
+require 'shopify-api-limits/shopify_api/shopify_api'
 
 module ShopifyAPI
   module Limits
