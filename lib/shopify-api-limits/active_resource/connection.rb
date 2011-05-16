204c057cae8402e70a72c6c737a6134d71b163a9
Chris Scott <christocracy@gmail.com>
2011-05-16 00:53:24 -0400

    Found a much safer way to hack into AR::Connection

--- lib/shopify-api-limits/active_resource/connection.rb
+++ lib/shopify-api-limits/active_resource/connection.rb
@@ -2,21 +2,14 @@ require 'active_resource'
 
 module ActiveResource
   class Connection
-    # HACK 1:  Add an attr_reader for response
+    # HACK:  Add an attr_reader for response
     attr_reader :response
-  
-    def request(method, path, *arguments)      
-      result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
-        payload[:method]      = method
-        payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
-        payload[:result]      = http.send(method, path, *arguments)
-      end  
-      # HACK 2:  Save response to instance var @response  
-      @response = handle_response(result)
-    rescue Timeout::Error => e
-      raise TimeoutError.new(e.message)
-    rescue OpenSSL::SSL::SSLError => e
-      raise SSLError.new(e.message)
-    end  
+    
+    # capture the original #handle_response as unbound method instead of using alias 
+    handle_response = self.instance_method(:handle_response)
+        
+    define_method(:handle_response) do |response|
+      @response = handle_response.bind(self).call(response)
+    end        
   end
 end
